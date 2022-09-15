9cc0d75e732844893672b7ca2e163806f6bc7bd2
hurtcopain <jacksummers715@gmail.com>
2022-09-14 20:29:03 -0400

    Project Zephyrus Raviole Bringup

--- oriole.mk
+++ oriole.mk
@@ -3,14 +3,15 @@ TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ANIMATIONS := true
+CUSTOM_BUILD_TYPE := Official
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_BLUR := true
+WITH_GMS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole_64.mk)
@@ -22,7 +23,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.gs101
     
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := derp_oriole
+PRODUCT_NAME := oriole
 PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := google
 
--- raven.mk
+++ raven.mk
@@ -3,14 +3,15 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ANIMATIONS := true
+CUSTOM_BUILD_TYPE := Official
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_BLUR := true
+WITH_GMS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven_64.mk)
@@ -22,7 +23,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.gs101
     
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := derp_raven
+PRODUCT_NAME := raven
 PRODUCT_MODEL := Pixel 6 Pro
 PRODUCT_BRAND := google
 
